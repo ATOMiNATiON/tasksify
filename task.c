2148c0ea33db36740fda945993a2d6f3af0681be
Adam Wu <adamwu@Adams-MacBook-Air-2.local>
2023-08-12 10:47:14 -0700

    now has ability to add and list out tasks

--- task.c
+++ task.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-
+#define MAX_BUFF 4096
 #define OPTIONS "ha:l"
 
 int main(int argc, char **argv) {
     int ch = 0, a = 0, l = 0, h = 0;
-    char *tskname;
+    char *taskname;
+    FILE *fp;
+    char buff[MAX_BUFF];
+
     while ((ch = getopt(argc, argv, OPTIONS)) != -1) {
         switch (ch) {
             case 'a':
                 a = 1;
-                tskname = optarg;
+                taskname = optarg;
                 break;
             case 'l':
                 l = 1;
@@ -21,11 +25,23 @@ int main(int argc, char **argv) {
     
     if (a && !h) {
         printf("Adding a task...\n");
-        printf("%s\n", tskname);
+        // only do appending to a file, not writing!
+        // can only append 1 item into file right now
+        fp = fopen("task.txt", "a");
+        fputs(taskname, fp);
+        fclose(fp); 
     }
     
     if (l && !h) {
         printf("Listing out all tasks...\n");
+        fp = fopen("task.txt", "r");
+        int tsknum = 1;
+        while (fgets(buff, sizeof(buff), fp)) {
+            printf("%d: %s", tsknum, buff);
+            tsknum++; 
+        }
+        printf("\n"); 
+        fclose(fp);
     }
     
     if (h) { 
