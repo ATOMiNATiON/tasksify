170b542d3f48424be0821572ba3536a1679e2e00
Adam Wu <adamwu@Adams-MacBook-Air-2.local>
2023-08-14 11:08:35 -0700

    sll.h file now contains basic functions for ADT

--- sll.h
+++ sll.h
@@ -12,5 +12,6 @@ List *list_create(const char *filename);
 void free_list(List **list);
 void list_tasks(List *list);
 void add_task(const char *taskname, List **list, const char *filename);
+void remove_task(int task_id, List **list);
 
 #endif
